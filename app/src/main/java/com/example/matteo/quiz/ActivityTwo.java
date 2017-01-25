60cc0c2d59a41b61d70d918aa94feef0e0780f39
indogz <lalavegna@gmail.com>
2017-01-25 22:33:02 +0100

    Manca da modificare le singole risposte date

--- app/src/main/java/com/example/matteo/quiz/ActivityTwo.java
+++ app/src/main/java/com/example/matteo/quiz/ActivityTwo.java
@@ -2,6 +2,7 @@ package com.example.matteo.quiz;
 
 import android.content.Context;
 import android.content.Intent;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -15,55 +16,172 @@ public class ActivityTwo extends AppCompatActivity {
 
     TextView result;
 
+    private final  String risposteCorrette = "1441";
+    String risposteDate = "";
+    Button btnTermina;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_two);
 
+        for (int i=0; i<risposteCorrette.length();i++){
+            risposteDate=risposteDate+" ";
+        }
 
-        Button btnTermina = (Button) findViewById(R.id.buttonTermina);
-        result= (TextView) findViewById(R.id.result);
+
+        btnTermina = (Button) findViewById(R.id.buttonTermina);
+        result = (TextView) findViewById(R.id.result);
         result.setEnabled(false);
 
         final Context context = getApplicationContext();
         final int duration = Toast.LENGTH_SHORT;
 
+
         btnTermina.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
-
-                Toast toast = Toast.makeText(context, "BTNTERMINA", duration);
-                toast.show();
+                if (risposteDate.equals(risposteCorrette)) {
+                    Toast toast = Toast.makeText(context, "CORRETTO", duration);
+                    toast.show();
+                } else {
+                    Toast toast = Toast.makeText(context, "SBAGLIATO", duration);
+                    toast.show();
+                }
             }
         });
     }
 
+
     public void onRadioButtonClicked(View view) {
         // Is the button now checked?
         boolean checked = ((RadioButton) view).isChecked();
+        RadioGroup myRadioGroup = (RadioGroup) findViewById(R.id.yourRadioGroup);
+        //int index = myRadioGroup.indexOfChild(findViewById(myRadioGroup.getCheckedRadioButtonId()));
+
 
         // Check which radio button was clicked
-        switch(view.getId()) {
-            case R.id.radio1:
-                if (checked)
-                    result.setText("Raio1");
+        switch (view.getId()) {
+            case R.id.radio11:
+                if (checked) {
+                    risposteDate = risposteDate + "1";
+                    result.setText(risposteDate);
                     result.setEnabled(true);
-                    break;
-            case R.id.radio2:
-                if (checked)
-                    result.setText("Raio2");
-                result.setEnabled(true);
-                    break;
-            case R.id.radio3:
-                if (checked)
-                    result.setText("Raio3");
-                result.setEnabled(true);
-                    break;
-            case R.id.radio4:
-                if (checked)
-                    result.setText("Raio4");
-                result.setEnabled(true);
-                    break;
+                }
+                break;
+            case R.id.radio12:
+                if (checked) {
+                    risposteDate = risposteDate + "2";
+
+                    result.setText(risposteDate);
+                    result.setEnabled(true);
+                }
+                break;
+            case R.id.radio13:
+                if (checked) {
+                    result.setText(risposteDate);
+                    result.setEnabled(true);
+                    risposteDate = risposteDate + "3";
+                }
+                break;
+            case R.id.radio14:
+                if (checked) {
+                    result.setText(risposteDate);
+                    result.setEnabled(true);
+                    risposteDate = risposteDate + "4";
+                }
+                break;
+            case R.id.radio21:
+                if (checked) {
+                    result.setText(risposteDate);
+                    result.setEnabled(true);
+                    risposteDate = risposteDate + "1";
+                }
+                break;
+            case R.id.radio22:
+                if (checked) {
+                    result.setText(risposteDate);
+                    result.setEnabled(true);
+                    risposteDate = risposteDate + "2";
+                }
+                break;
+            case R.id.radio23:
+                if (checked) {
+                    result.setText(risposteDate);
+                    result.setEnabled(true);
+                    risposteDate = risposteDate + "3";
+                }
+                break;
+            case R.id.radio24:
+                if (checked) {
+                    result.setText(risposteDate);
+                    result.setEnabled(true);
+                    risposteDate = risposteDate + "4";
+                }
+                break;
+            case R.id.radio31:
+                if (checked) {
+                    result.setText(risposteDate);
+                    result.setEnabled(true);
+                    risposteDate = risposteDate + "1";
+                }
+                break;
+            case R.id.radio32:
+                if (checked) {
+                    result.setText(risposteDate);
+                    result.setEnabled(true);
+                    risposteDate = risposteDate + "2";
+
+                }
+                break;
+            case R.id.radio33:
+                if (checked) {
+                    result.setText(risposteDate);
+                    result.setEnabled(true);
+                    risposteDate = risposteDate + "3";
+                }
+                break;
+            case R.id.radio34:
+                if (checked) {
+                    result.setText(risposteDate);
+                    result.setEnabled(true);
+                    risposteDate = risposteDate + "4";
+                }
+                break;
+            case R.id.radio41:
+                if (checked) {
+                    result.setText(risposteDate);
+                    result.setEnabled(true);
+                    risposteDate = risposteDate + "1";
+                }
+                break;
+            case R.id.radio42:
+                if (checked) {
+                    result.setText(risposteDate);
+                    result.setEnabled(true);
+                    risposteDate = risposteDate + "2";
+                }
+                break;
+            case R.id.radio43:
+                if (checked) {
+                    result.setText(risposteDate);
+                    result.setEnabled(true);
+                    risposteDate = risposteDate + "3";
+                }
+                break;
+            case R.id.radio44:
+                if (checked) {
+                    result.setText(risposteDate);
+                    result.setEnabled(true);
+                    risposteDate = risposteDate + "4";
+                }
+                break;
         }
+
+        result.setText(risposteDate);
+
     }
 }
+
